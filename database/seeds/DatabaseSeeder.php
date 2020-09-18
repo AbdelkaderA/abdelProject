d0b6fe5bdeefd5d824d85156bdc4a09bfbd538a0
amine <58747816+devweb-amine@users.noreply.github.com>
2020-09-18 15:01:29 +0200

    PhpLaravel1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        App\User::create(
+            [
+                'name' => 'abdel',
+                'email' => 'jhgdsfnt@la.fr',
+                'password' => bcrypt('pass'),
+            ]
+        );
     }
 }
